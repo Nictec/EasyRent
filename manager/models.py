b17e062d0dd902974a3e030fd302f78d8367b1be
Nictec <nicnac9911@gmail.com>
2017-03-04 18:02:40 +0100

    fixed markdown error

--- manager/models.py
+++ manager/models.py
@@ -5,61 +5,61 @@ from .choices import *
 
 # Create your models here.
 class Equipment(models.Model):
-    name = models.CharField(max_length=30)
-    fabricator = models.CharField(max_length=30, default='-')
-    beschreibung = models.CharField(max_length=1000, blank=True)
-    labor = models.CharField(max_length=1, choices=labor_choices)
-    max_quantity = models.IntegerField(default=1, null = True)
-    #avail_quantity = models.IntegerField (default=1, null = True)
-    status = models.CharField(max_length=8, choices = STATUS_CHOICES, default = 'im Lager')
-    storeplace = models.ForeignKey("shelf", on_delete=models.CASCADE)
-    rent_price = models.IntegerField(null=True)
-    purchasing_price = models.IntegerField(null=True)
-    features = models.TextField(null=True)
-    def __str__(self):
-        return self.name
+	name = models.CharField(max_length=30)
+	fabricator = models.CharField(max_length=30, default='-')
+	beschreibung = models.CharField(max_length=1000, blank=True)
+	labor = models.CharField(max_length=1, choices=labor_choices)
+	max_quantity = models.IntegerField(default=1, null = True)
+	#avail_quantity = models.IntegerField (default=1, null = True)
+	status = models.CharField(max_length=8, choices = STATUS_CHOICES, default = 'im Lager')
+	storeplace = models.ForeignKey("shelf", on_delete=models.CASCADE)
+	rent_price = models.IntegerField(null=True)
+	purchasing_price = models.IntegerField(null=True)
+	features = models.TextField(null=True, default="")
+	def __str__(self):
+		return self.name
 
 
 class Order(models.Model):
-    name = models.CharField(max_length=30)
-    Type = models.CharField(
-        max_length=2,
-        choices=TYPE_CHOICES,
-        default='Rental',
-        )
-    city = models.CharField(max_length=30, null=True, blank=True)
-    street= models.CharField(max_length=30, null=True, blank=True)
-    dateStart = models.DateField(null=True)
-    dateEnd = models.DateField(null=True)
-    GuestNumber = models.IntegerField(null=True)
-    description = models.TextField()
-    status = models.CharField(max_length=30, choices=order_choices, default='notOK')
-    client = models.ForeignKey("client", on_delete=models.CASCADE, blank = True, null = True)
-    assignments = models.ManyToManyField('Equipment', blank = True, through= 'Assignment', through_fields=('order', 'equipment'))
+	name = models.CharField(max_length=30)
+	Type = models.CharField(
+		max_length=2,
+		choices=TYPE_CHOICES,
+		default='Rental',
+		)
+	city = models.CharField(max_length=30, null=True, blank=True)
+	street= models.CharField(max_length=30, null=True, blank=True)
+	dateStart = models.DateField(null=True)
+	dateEnd = models.DateField(null=True)
+	GuestNumber = models.IntegerField(null=True)
+	description = models.TextField()
+	status = models.CharField(max_length=30, choices=order_choices, default='notOK')
+	client = models.ForeignKey("client", on_delete=models.CASCADE, blank = True, null = True)
+	assignments = models.ManyToManyField('Equipment', blank = True, through= 'Assignment', through_fields=('order', 'equipment'))
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
 class Assignment(models.Model):
-    order = models.ForeignKey('Order', related_name = "assignment")
-    equipment = models.ForeignKey('Equipment')
-    quantity = models.PositiveIntegerField(default=1)
+	order = models.ForeignKey('Order', related_name = "assignment")
+	equipment = models.ForeignKey('Equipment')
+	quantity = models.PositiveIntegerField(default=1)
 
 class Shelf(models.Model):
-    name = models.CharField(max_length=100)
-    row = models.IntegerField(null=True)
-    sNr = models.IntegerField(null=True)
-    def __str__(self):
-        return self.name
+	name = models.CharField(max_length=100)
+	row = models.IntegerField(null=True)
+	sNr = models.IntegerField(null=True)
+	def __str__(self):
+		return self.name
 
 class Client(models.Model):
-    firstname = models.CharField(max_length=30)
-    secondname = models.CharField(max_length=30)
-    email = models.EmailField()
-    post_code = models.IntegerField()
-    city = models.CharField(max_length=30)
-    street= models.CharField(max_length=30)
+	firstname = models.CharField(max_length=30)
+	secondname = models.CharField(max_length=30)
+	email = models.EmailField()
+	post_code = models.IntegerField()
+	city = models.CharField(max_length=30)
+	street= models.CharField(max_length=30)
 
-    def __str__(self):
-        return "%s %s" % (self.firstname, self.secondname)
+	def __str__(self):
+		return "%s %s" % (self.firstname, self.secondname)
 
