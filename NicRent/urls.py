0b14dc1f5d158b6ee72c5bc2a44d8f6cbd7cbfda
Nicholas Lamprecht <nl@x-net.at>
2017-06-01 22:53:08 +0200

    finished the password reset view

--- NicRent/urls.py
+++ NicRent/urls.py
@@ -11,7 +11,7 @@ from rest_framework.authtoken import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', views.obtain_auth_token),
-    url(r'^users/', include('users.urls')),
+    url(r'^accounts/', include('users.urls')),
     url(r'^', include('manager.urls'), name="manager"),
 
 ]
--- users/views.py
+++ users/views.py
@@ -19,7 +19,7 @@ from django.db.models.query_utils import Q
 class ResetPasswordRequestView(FormView):
     template_name = "reset_request.html"    #code for template is given below the view's code
     form_class = PasswordResetRequestForm
-    success_url = '/users/reset_password/'
+    success_url = '/accounts/reset_password/'
 
     @staticmethod
     def validate_email_address(email):
@@ -49,7 +49,7 @@ class ResetPasswordRequestView(FormView):
                         c = Context({
                             'email': user.email,
                             'domain': request.META['HTTP_HOST'],
-                            'site_name': 'your site',
+                            'site_name': 'api.nictec.at',
                             'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                             'user': user,
                             'token': default_token_generator.make_token(user),
@@ -57,8 +57,8 @@ class ResetPasswordRequestView(FormView):
                             })
                         subject_template_name='password_reset_subject.txt'
                         # copied from django/contrib/admin/templates/registration/password_reset_subject.txt to templates directory
-                        text_template_name='text_email.txt'
-                        html_template_name='password_reset_email.html'
+                        text_template_name='email/text_email.txt'
+                        html_template_name='email/password_reset_email.html'
                         # copied from django/contrib/admin/templates/registration/password_reset_email.html to templates directory
                         subject = loader.render_to_string(subject_template_name, c)
                         # Email subject *must not* contain newlines
@@ -84,22 +84,31 @@ class ResetPasswordRequestView(FormView):
             associated_users= User.objects.filter(username=data)
             if associated_users.exists():
                 for user in associated_users:
-                    c = {
-                        'email': user.email,
-                        'domain': 'example.com', #or your domain
-                        'site_name': 'example',
-                        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                        'user': user,
-                        'token': default_token_generator.make_token(user),
-                        'protocol': 'http',
-                        }
-                    subject_template_name='password_reset_subject.txt'
-                    email_template_name='password_reset_email.html'
-                    subject = loader.render_to_string(subject_template_name, c)
-                    # Email subject *must not* contain newlines
-                    subject = ''.join(subject.splitlines())
-                    email = loader.render_to_string(email_template_name, c)
-                    send_mail(subject, email, settings.DEFAULT_FROM_EMAIL , [user.email], fail_silently=False)
+                        c = Context({
+                            'email': user.email,
+                            'domain': request.META['HTTP_HOST'],
+                            'site_name': 'api.nictec.at',
+                            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                            'user': user,
+                            'token': default_token_generator.make_token(user),
+                            'protocol': 'http',
+                            })
+                        subject_template_name='password_reset_subject.txt'
+                        # copied from django/contrib/admin/templates/registration/password_reset_subject.txt to templates directory
+                        text_template_name='email/text_email.txt'
+                        html_template_name='email/password_reset_email.html'
+                        # copied from django/contrib/admin/templates/registration/password_reset_email.html to templates directory
+                        subject = loader.render_to_string(subject_template_name, c)
+                        # Email subject *must not* contain newlines
+                        subject = ''.join(subject.splitlines())
+                        plain_text = get_template(text_template_name)
+                        htmly = get_template(html_template_name)
+                        text_content = plain_text.render(c)
+                        html_content = htmly.render(c)
+                        msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [user.email])
+                        msg.attach_alternative(html_content, "text/html")
+                        msg.content_subtype = "html"  # Main content is now text/html
+                        msg.send()
                 result = self.form_valid(form)
                 messages.success(request, "Email wurde verschickt. Bitte überprüfen Sie Ihren Posteingang")
                 return result
@@ -110,7 +119,7 @@ class ResetPasswordRequestView(FormView):
         return self.form_invalid(form)
 
 class PasswordResetConfirmView(FormView):
-    template_name = "reset_request.html"
+    template_name = "reset_new.html"
     success_url = 'http://localhost:8080/#/'
     form_class = SetPasswordForm
 
